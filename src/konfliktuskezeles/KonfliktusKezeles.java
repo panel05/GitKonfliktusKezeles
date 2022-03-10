3ec4aa710e15a39fc56a25e146d7f460b1d8d23a
panel05 <pantlbalazs@gmail.com>
2022-03-10 12:29:10 +0100

    1. név inicializálása

--- src/konfliktuskezeles/KonfliktusKezeles.java
+++ src/konfliktuskezeles/KonfliktusKezeles.java
@@ -1,7 +1,7 @@
 package konfliktuskezeles;
 public class KonfliktusKezeles {
     public static void main(String[] args) {
-        String nev1;
+        String nev1 = "PAB";
         String nev2;
     }
     
